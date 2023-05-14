a015a77663079da4c5856695aa99e6a2f31e077c
lag-packet <satarhassni0@gmail.com>
2023-05-14 06:04:36 -0400

    weatherCommand base

--- app/commands/weather/weatherCommand.js
+++ app/commands/weather/weatherCommand.js
@@ -1,7 +1,20 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { getWeatherData } = require('../../weatherApi.js');
+const { OPENWEATHERMAP_API_KEY } = require('../../config.json');
 
 module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('weather')
+		.setDescription('Provides weather information.'),
+	async execute(interaction) {
+		await interaction.reply(`Weather command!`);
+	},
+};
+
+
+
+
+/*{
 	data: new SlashCommandBuilder()
 		.setName('weather')
 		.setDescription('Provides weather information for the specified city.')
@@ -12,7 +25,7 @@ module.exports = {
 	async execute(interaction) {
 		const city = interaction.options.getString('city');
 		// Fetch the weather data for the given city using your getWeatherData function
-		//const weatherData = await getWeatherData(city);
+		const weatherData = await getWeatherData(city, OPENWEATHERMAP_API_KEY);
 		return interaction.reply(`Weather data for ${city}: ${weatherData}`);
 	},
-};
+};*/
--- deploy-commands.js
+++ deploy-commands.js
@@ -1,11 +1,11 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, guildId, token } = require('./app/config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, 'app/commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
@@ -17,6 +17,7 @@ for (const folder of commandFolders) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
 		if ('data' in command && 'execute' in command) {
+			console.log(`registered: ${command.data.name}`);
 			commands.push(command.data.toJSON());
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
