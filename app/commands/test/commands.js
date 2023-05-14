08174f45f6c6b556505a0a2acafa95e7c0d43855
Satar Hassni <satarhassni@macbook-pro.myfiosgateway.com>
2023-05-14 04:01:14 -0400

    weatherLocation command

--- app/commands/test/commands.js
+++ app/commands/test/commands.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('server')
+		.setName('weather server')
 		.setDescription('Provides information about the server.'),
 	async execute(interaction) {
 		// interaction.guild is the object representing the Guild in which the command was run
